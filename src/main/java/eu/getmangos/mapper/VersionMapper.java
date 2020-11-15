664b17e35fa26b2aaa0e1ce5075fa20d3cccc7b1
Warkdev <cedric.servais@outlook.com>
2020-11-15 14:51:33 +0100

    Update mapping properties

--- src/main/java/eu/getmangos/mapper/VersionMapper.java
+++ src/main/java/eu/getmangos/mapper/VersionMapper.java
@@ -1,6 +1,8 @@
 package eu.getmangos.mapper;
 
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import eu.getmangos.dto.DbVersionDTO;
 import eu.getmangos.entities.DbVersion;
@@ -8,8 +10,12 @@ import eu.getmangos.entities.DbVersion;
 @Mapper(componentModel = "cdi")
 public interface VersionMapper {
 
+    @Mapping(source = "version.id.version", target = "version")
+    @Mapping(source = "version.id.structure", target = "structure")
+    @Mapping(source = "version.id.content", target = "content")
     DbVersionDTO versionToDTO(DbVersion version);
 
+    @InheritInverseConfiguration
     DbVersion dtoToEntity(DbVersionDTO version);
 
 }
